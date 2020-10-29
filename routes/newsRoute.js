8d1eba7138e5672c8d4f8a95d33b083d78b354a7
wellytansil <we2y.tansil@gmail.com>
2020-10-29 11:25:05 +0700

    newsapi,covidapi backend done

--- routes/newsRoute.js
+++ routes/newsRoute.js
@@ -1,7 +1,9 @@
 'use strict'
 const express = require('express');
-const newsRoute = express.Router()
+const router = express.Router()
+const NewsController = require('../controllers/NewsController')
+
+router.get('/', NewsController.accessNews)
 
 
-
-module.exports = newsRoute
+module.exports = router
